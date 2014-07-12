6d7a550be2326921861f0586ba10f41141872705
zzz6519003 <zhengzhongzhao@gmail.com>
2014-07-13 02:28:11 +0800

    connecting mongodb

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var MongoStore = require('connect-mongo')(express);
+var settings = require('./settings');
 
 var app = express();
 
@@ -23,6 +25,16 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db
+  })
+}));
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
