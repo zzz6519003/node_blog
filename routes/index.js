8dc5ed98e8ce2a39dc8384a0afa3c50414785e00
zzz6519003 <zhengzhongzhao@gmail.com>
2014-07-14 03:18:14 +0800

    add login logout

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,32 @@
 
 
 module.exports = function(app) {
-  app.get('/', function (req, res) {
-    res.render('index', { title: '主页' });
+  // app.get('/', function (req, res) {
+  //   res.render('index', { title: '主页' });
+  // });
+
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: '主页',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
   });
-  app.get('/reg', function (req, res) {
-    res.render('reg', { title: '注册' });
+});
+
+  // app.get('/reg', function (req, res) {
+  //   res.render('reg', { title: '注册' });
+  // });
+
+app.get('/reg', function (req, res) {
+  res.render('reg', {
+    title: '注册',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
   });
+});
+
   // app.post('/reg', function (req, res) {
   // });
 
@@ -53,16 +73,57 @@ app.post('/reg', function (req, res) {
   });
 });
 
-  app.get('/login', function (req, res) {
-    res.render('login', { title: '登录' });
+  // app.get('/login', function (req, res) {
+  //   res.render('login', { title: '登录' });
+  // });
+
+app.get('/login', function (req, res) {
+    res.render('login', {
+        title: '登录',
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()});
+});
+
+
+
+app.post('/login', function (req, res) {
+  //生成密码的 md5 值
+  var md5 = crypto.createHash('md5'),
+      password = md5.update(req.body.password).digest('hex');
+  //检查用户是否存在
+  User.get(req.body.name, function (err, user) {
+    if (!user) {
+      req.flash('error', '用户不存在!'); 
+      return res.redirect('/login');//用户不存在则跳转到登录页
+    }
+    //检查密码是否一致
+    if (user.password != password) {
+      req.flash('error', '密码错误!'); 
+      return res.redirect('/login');//密码错误则跳转到登录页
+    }
+    //用户名密码都匹配后，将用户信息存入 session
+    req.session.user = user;
+    req.flash('success', '登陆成功!');
+    res.redirect('/');//登陆成功后跳转到主页
   });
-  app.post('/login', function (req, res) {
-  });
+});
+
+
+  // app.post('/login', function (req, res) {
+  // });
   app.get('/post', function (req, res) {
     res.render('post', { title: '发表' });
   });
   app.post('/post', function (req, res) {
   });
-  app.get('/logout', function (req, res) {
-  });
+  // app.get('/logout', function (req, res) {
+  // });
+
+app.get('/logout', function (req, res) {
+  req.session.user = null;
+  req.flash('success', '登出成功!');
+  res.redirect('/');//登出成功后跳转到主页
+});
+
 };
